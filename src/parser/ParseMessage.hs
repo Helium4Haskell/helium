a20311901327bd07e0560fc657609abbd58be528
uust <unknown@mail>
2003-04-05 11:00:49 +0000

    Arjan

--- src/parser/ParseMessage.hs
+++ src/parser/ParseMessage.hs
@@ -23,37 +23,3 @@ instance HasMessage ParseError where
             position = Position_Position name line col
         in [ Range_Range position position ]
 
-{-
-sms msgs = show (map sm msgs)
-  where
-     sm (SysUnExpect s) = "SysUnExpect" ++ s
-     sm (UnExpect    s) = "UnExpect" ++ s
-     sm (Expect      s) = "Expect" ++ s
-     sm (Message     s) = "Message" ++ s
-
-specialCase :: [Message] -> Maybe [String]
-specialCase msgs 
-    | not (null charTexts) = Just
-            [ head charTexts
-            , "correct examples of character literals: 'a', '\\n', '%', ' ', '\\''"
-            ]
-    | not (null stringTexts) = Just
-        [ head stringTexts
-        , "correct examples of string literals: \"Helium\", \"10\", \"\", \"fun\\nny\\\"\""
-        ]
-    | not (null floatTexts) = Just
-        [ head floatTexts
-        , "correct examples of float-point literals: 1.0, 3.14159, 100.8, 0.00001"
-        ]
-
-    | not (null commentTexts) = Just
-        [ head commentTexts ]
-
-    where
-        strings      = map messageString msgs 
-        charTexts    = [ t | t <- charErrors, t `elem` strings ] 
-        stringTexts  = [ t | t <- stringErrors, t `elem` strings ]
-        floatTexts   = [ t | t <- floatErrors, t `elem` strings ]
-        commentTexts = [ t | t <- commentErrors, t `elem` strings ]
-specialCase _ = Nothing
--}
