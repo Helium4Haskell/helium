94b29ebb6213b774baf46d27d2a5e41e47c05022
uust <unknown@mail>
2003-04-10 13:52:45 +0000

    Positions of parse error because of layout are better now. x = y = 3 We now point to the end of "x =" instead of to the beginning of "y=3" Arjan

--- src/parser/LayoutRule.hs
+++ src/parser/LayoutRule.hs
@@ -5,17 +5,18 @@ import ParsecPos
 
 layout :: [Token] -> [Token]
 layout [] = []
-layout input@((pos, lexeme):_) = fixEOF . optimise $
+layout input@((pos, lexeme):_) = optimise $
     case lexeme of 
         LexKeyword "module" -> 
-            lay zeroPos [] input
+            lay dummyToken [] input
         LexSpecial '{' ->
-            lay zeroPos [] input
+            lay dummyToken [] input
         _ ->
             (pos, LexInsertedOpenBrace) : 
-            lay zeroPos [CtxLay (sourceColumn pos) False] input
+            lay dummyToken [CtxLay (sourceColumn pos) False] input
     where
         zeroPos = setSourceColumn (setSourceLine pos 0) 0
+        dummyToken = (zeroPos, LexVar "")
 
 optimise :: [Token] -> [Token]
 optimise (token1@(_, LexInsertedOpenBrace) : (_, LexInsertedSemicolon) : ts) =
@@ -29,9 +30,9 @@ data Context
     | CtxBrace
     deriving (Eq,Show)
     
---  position previous token
---            enclosing contexts
-lay :: SourcePos -> [Context] -> [Token] -> [Token]
+--     previous token
+--              enclosing contexts
+lay :: Token -> [Context] -> [Token] -> [Token]
 
 -- If we're in a CtxBrace and we see a '}', we leave that context.
 -- If we see another token, we check to see if we need to add a
@@ -40,76 +41,64 @@ lay     _
         cc@(CtxBrace:cs) 
         input@(t@(pos, lexeme):ts)
     | lexeme == LexSpecial '}' =
-        t : lay pos cs ts
+        t : lay t cs ts
     | otherwise = 
-        t : addContext pos cc input 
+        t : addContext t cc input 
 
 -- If we're in a let layout context, an 'in' can end
 -- the context, too.
-lay     _ 
+lay     prevToken 
         (CtxLay _ True:cs) 
         (t@(pos, LexKeyword "in"):ts) 
-    = (pos, LexInsertedCloseBrace) : t : lay pos cs ts
+    = (behind prevToken, LexInsertedCloseBrace) : t : lay t cs ts
 
 -- If we're in a layout context and the new token is not on the 
 -- same line as the previous, we check the column against the
 -- context. If the new token is on the same line, we only need
 -- to check whether a context has to be added.
-lay     prevPos 
+lay     prevToken@(prevPos, _)
         cc@(CtxLay ctxCol _:cs) 
         input@(t@(pos, _):_) 
     | sourceLine pos > sourceLine prevPos = -- token on next line?
         if sourceColumn pos > ctxCol then -- indent more => nothing
-            t : addContext pos cc input
+            t : addContext t cc input
         else if sourceColumn pos == ctxCol then -- indent same => insert ';' 
-            (pos, LexInsertedSemicolon) : t : addContext pos cc input
+            (behind prevToken, LexInsertedSemicolon) : t : addContext t cc input
         else -- indent less => insert brace, remove context and try again
-            (pos, LexInsertedCloseBrace) : lay prevPos cs input
+            (behind prevToken, LexInsertedCloseBrace) : lay prevToken cs input
     | otherwise = -- token on same line
-        t : addContext pos cc input
-
+        t : addContext t cc input
+        
 lay _ _ [] = []
 
 lay _ [] input@(t@(pos, _):_) = 
-    t : addContext pos [] input
+    t : addContext t [] input
 
-addContext :: SourcePos -> [Context] -> [Token] -> [Token]
+behind :: Token -> SourcePos
+behind (pos, lexeme) = incSourceColumn pos (lexemeLength lexeme)
+
+addContext :: Token -> [Context] -> [Token] -> [Token]
 
 -- If we see a '{' we add a CtxBrace context
-addContext prevPos cs ((_, LexSpecial '{'):ts) = 
-    lay prevPos (CtxBrace : cs) ts
+addContext prevToken cs ((_, LexSpecial '{'):ts) = 
+    lay prevToken (CtxBrace : cs) ts
 
 -- If we see a 'do', 'where', 'of' or 'let' we add a context
 -- and a '{' only if the next token is not a '{'
-addContext prevPos cs 
+addContext prevToken cs 
         ((_, LexKeyword keyword):t2@(pos2, lexeme2):ts) 
     | keyword `elem` ["do", "where", "of","let"] =
         if lexeme2 == LexSpecial '{' then
-            lay prevPos cs (t2:ts)
+            lay prevToken cs (t2:ts)
         else
             (pos2, LexInsertedOpenBrace) :
-            lay prevPos 
+            lay prevToken 
                 (CtxLay (sourceColumn pos2) (keyword == "let") : cs) 
                 (t2:ts)
     | otherwise = 
-        lay prevPos cs (t2:ts)
+        lay prevToken cs (t2:ts)
 
-addContext prevPos cs (_:ts) =
-    lay prevPos cs ts
+addContext prevToken cs (_:ts) =
+    lay prevToken cs ts
 
 addContext _ _ [] = []
-
--- Make EOF's position equal to the end of the last token before EOF
-fixEOF :: [Token] -> [Token]
-fixEOF tokens 
-    | null revOthers = tokens
-    | otherwise = reverse $
-                        (newEOFPos, LexEOF) 
-                      : (  map (\(_, lexeme) -> (newEOFPos, lexeme)) revSamePos
-                        ++ revOthers
-                        )
-    where
-        newEOFPos = incSourceColumn lastPos (lexemeLength lastLexeme)
-        (lastPos, lastLexeme) = head revOthers
-        (revSamePos, revOthers) = span ((== pos) . fst) revTokens
-        ((pos, LexEOF):revTokens) = reverse tokens
