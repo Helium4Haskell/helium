9dd7418671835c346f711d8a4f8b689d5f9c82de
uust <unknown@mail>
2003-05-12 08:47:44 +0000

    Nieuwe .out files (voor nieuwe 'type inferentie heuristieken')    - Bastiaan

--- test/thompson/Thompson14.hs
+++ test/thompson/Thompson14.hs
@@ -2,13 +2,3 @@ double :: Int -> Int
 double x = 2 * x
 
 main = double -4
-
-{-
-(4,8): Type error in infix application
-*** Expression     : double - 4
-*** Term           : double
-*** Type           : Int -> Int
-*** Does not match : Int       
-
-iK ZOU GRAAG ZIEN DAT - VAN TYPE INT->INT->INT IS. 
--}
