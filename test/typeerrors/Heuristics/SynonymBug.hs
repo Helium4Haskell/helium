6f715f1e66e09b1f733b1a72651efe0ec61771c3
uust <unknown@mail>
2003-10-22 13:56:28 +0000

    Nieuwe out files !   - Arjan & Bastiaan

--- test/typeerrors/Heuristics/SynonymBug.hs
+++ test/typeerrors/Heuristics/SynonymBug.hs
@@ -1,8 +1,8 @@
 -- Ulf Norell, 24 september 2003
-type Fun = Int -> Int                                                                                                         
-                                                                                                                              
-foo :: Fun                                                                                                                  
-foo x = x                                                                                                                     
-                                                                                                                              
-bar :: Int                                                                                                                    
+type Fun = Int -> Int
+
+foo :: Fun
+foo x = x
+
+bar :: Int
 bar = foo [3]
