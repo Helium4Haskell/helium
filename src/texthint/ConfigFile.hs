d01d9eea3a58ba2547a4e1aff66dbd5aa65de205
Jurriaan Hage <J.Hage@uu.nl>
2008-02-11 11:55:37 +0000

    The easiest thing to do: add a new file, but forget to add it to the repo.

---
+++ src/texthint/ConfigFile.hs
@@ -0,0 +1,54 @@
+module ConfigFile (Config, readConfig) where
+
+import Char
+import Control.Monad
+import qualified Data.Map as Map
+import Text.ParserCombinators.Parsec
+import Data.Either
+import Data.Maybe
+
+type Config = [(String,String)]
+
+-- Thanks to Bryan OÕSullivan, I might upgrade this later to something more in
+-- the style of the Helium parser.
+ident :: Parser String
+ident = do c  <- letter <|> char '_'
+           cs <- many (letter <|> digit <|> char '_')
+           return (c:cs)
+        <?> "identifier"
+ 
+comment :: Parser ()
+comment = do char '#'
+             skipMany (noneOf "\r\n")
+        <?> "comment"
+
+eol = do oneOf "\n\r"
+         return ()
+      <?> "end of line"
+
+item :: Parser (String, String)
+item = do key <- ident
+          skipMany space
+          char '='
+          value <- many (noneOf "\n\r")
+          newline
+          return (key, strip value)
+    where strip = reverse . dropWhile isSpace . reverse . dropWhile isSpace 
+
+line :: Parser (Maybe (String, String))
+line = do skipMany space
+          try (comment >> return Nothing) <|> (item >>= return . Just)
+          
+file :: Parser [(String, String)]
+file = do lines <- many line
+          return (catMaybes lines)
+
+readConfig :: SourceName -> IO Config
+readConfig name = do{ result <- parseFromFile file name
+                    ; case (result) of
+                       Left err  -> do{ putStrLn (show err)
+                                      ; return []
+                                      }
+                       Right xs  -> return (reverse xs) 
+                    }
+
