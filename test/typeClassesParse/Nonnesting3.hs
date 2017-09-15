4b12768ee37f607410513c810061175a4222098f
Jurriaan Hage <J.Hage@uu.nl>
2017-09-15 10:07:03 +0000

    Whatever

--- test/typeClassesParse/Nonnesting3.hs
+++ test/typeClassesParse/Nonnesting3.hs
@@ -1,4 +1,8 @@
 -- Fixity declarations cannot be inside a class scope
+-- The language report in fact says they can as long as they
+-- are about operators defined in the class. Since these fixity
+-- decls can also be at top-level, I have decided not to allow
+-- them and keep things simple in a class.
 
 class W a where
   infixr 5 ++
