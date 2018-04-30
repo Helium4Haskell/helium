94b4ab6d67e72a82015f9e517faf7780655a8ce5
Reinier Maas <ReinierMaas@hotmail.com>
2018-04-30 09:44:23 +0200

    trailing spaces

--- src/Helium/Main/PhaseTypeInferencer.hs
+++ src/Helium/Main/PhaseTypeInferencer.hs
@@ -17,8 +17,8 @@ import Helium.StaticAnalysis.Messages.TypeErrors
 import Helium.StaticAnalysis.Messages.Information (showInformation)
 import System.FilePath.Posix
 
-phaseTypeInferencer :: 
-    String -> String -> Module -> ImportEnvironment -> ImportEnvironment -> [Option] -> 
+phaseTypeInferencer ::
+    String -> String -> Module -> ImportEnvironment -> ImportEnvironment -> [Option] ->
     Phase TypeError (DictionaryEnvironment, ImportEnvironment, TypeEnvironment, [Warning])
 
 phaseTypeInferencer basedir fullName module_ localEnv completeEnv options = do
@@ -26,41 +26,41 @@ phaseTypeInferencer basedir fullName module_ localEnv completeEnv options = do
 
     -- 'W' and 'M' are predefined type inference algorithms
     let newOptions = (if AlgorithmW `elem` options
-                        then filter (/= NoSpreading) . ([TreeWalkInorderTopLastPost, SolverGreedy]++) 
+                        then filter (/= NoSpreading) . ([TreeWalkInorderTopLastPost, SolverGreedy]++)
                         else id)
                    . (if AlgorithmM `elem` options
-                        then filter (/= NoSpreading) . ([TreeWalkInorderTopFirstPre, SolverGreedy]++)  
+                        then filter (/= NoSpreading) . ([TreeWalkInorderTopFirstPre, SolverGreedy]++)
                         else id)
                    $ options
-                   
+
         (debugIO, dictionaryEnv, toplevelTypes, typeErrors, warnings) =
            typeInferencing newOptions completeEnv module_
 
         -- add the top-level types (including the inferred types)
         finalEnv = addToTypeEnvironment toplevelTypes completeEnv
-    
-    when (DumpTypeDebug `elem` options) debugIO      
-     
+
+    when (DumpTypeDebug `elem` options) debugIO
+
     -- display name information
     showInformation True options finalEnv
-    
-    case typeErrors of 
-       
+
+    case typeErrors of
+
        _:_ ->
           do when (DumpInformationForAllModules `elem` options) $
                 putStr (show completeEnv)
              return (Left typeErrors)
-          
-       [] -> 
+
+       [] ->
           do -- Dump information
-             when (DumpInformationForAllModules `elem` options) $ 
+             when (DumpInformationForAllModules `elem` options) $
                 print finalEnv
              when (HFullQualification `elem` options) $
-                writeFile (combinePathAndFile basedir (dropExtension $ takeFileName fullName) ++ ".fqn") 
+                writeFile (combinePathAndFile basedir (dropExtension $ takeFileName fullName) ++ ".fqn")
                           (holmesShowImpEnv module_ finalEnv)
-             when (  DumpInformationForThisModule `elem` options 
+             when (  DumpInformationForThisModule `elem` options
                   && DumpInformationForAllModules `notElem` options
-                  ) 
+                  )
                   $ print (addToTypeEnvironment toplevelTypes localEnv)
-                  
+
              return (Right (dictionaryEnv, finalEnv, toplevelTypes, warnings))
