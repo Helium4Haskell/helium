08a57ccaf3da97fe5693edc296e3ac8cbe85ef90
Mias van Klei <miasvanklei@outlook.com>
2019-10-12 16:16:35 +0200

    DerivingEq.hs: add type to default, simple datatypes can now derive Eq

--- src/Helium/CodeGeneration/DerivingEq.hs
+++ src/Helium/CodeGeneration/DerivingEq.hs
@@ -25,37 +25,58 @@ typeDictEq = Core.TCon $ Core.TConTypeClassDictionary $ idFromString "Eq"
 
 -- Eq Dictionary for a data type declaration
 dataDictionary :: UHA.Declaration -> CoreDecl
-dataDictionary (UHA.Declaration_Data _ _ (UHA.SimpleType_SimpleType _ name names) constructors _) = DeclValue
-    { declName    = idFromString ("$dictEq$" ++ getNameName name)
-    , declAccess  = public
-    , declType    = foldr (\(typeArg, idx) -> Core.TForall (Core.Quantor idx $ Just $ getNameName typeArg) Core.KStar)
-                          (Core.typeFunction argTypes dictType)
-                        $ zip names [1 ..]
-    , valueValue  = eqDict dictType dataType names constructors
-    , declCustoms = [custom "type" ("Dict$Eq " ++ getNameName name)]
-                    ++ map (custom "typeVariable" . getNameName)                   names
-                    ++ map (\n -> custom "superInstance" ("Eq-" ++ getNameName n)) names
-    }
+dataDictionary (UHA.Declaration_Data _ _ (UHA.SimpleType_SimpleType _ name names) constructors _)
+    = DeclValue
+        { declName    = idFromString ("$dictEq$" ++ getNameName name)
+        , declAccess  = public
+        , declType    = foldr
+                                (\(typeArg, idx) -> Core.TForall
+                                    (Core.Quantor idx $ Just $ getNameName typeArg)
+                                    Core.KStar
+                                )
+                                (Core.typeFunction argTypes dictType)
+                            $ zip names [1 ..]
+        , valueValue  = eqDict dictType dataType names constructors
+        , declCustoms =
+            [custom "type" ("Dict$Eq " ++ getNameName name)]
+            ++ map (custom "typeVariable" . getNameName)                   names
+            ++ map (\n -> custom "superInstance" ("Eq-" ++ getNameName n)) names
+        }
   where
     argTypes :: [Core.Type]
-    argTypes = zipWith (\_ idx -> Core.TAp typeDictEq $ Core.TVar idx) names [0 ..]
+    argTypes =
+        zipWith (\_ idx -> Core.TAp typeDictEq $ Core.TVar idx) names [0 ..]
     dictType = Core.TAp typeDictEq dataType
-    dataType = Core.typeApplyList (Core.TCon $ Core.typeConFromString $ getNameName name)
-        $ zipWith (\_ idx -> Core.TVar idx) names [1 ..]
-dataDictionary _ = error "pattern match failure in CodeGeneration.Deriving.dataDictionary"
+    dataType =
+        Core.typeApplyList
+                (Core.TCon $ Core.typeConFromString $ getNameName name)
+            $ zipWith (\_ idx -> Core.TVar idx) names [1 ..]
+dataDictionary _ =
+    error "pattern match failure in CodeGeneration.Deriving.dataDictionary"
 
 eqDict :: Core.Type -> Core.Type -> [UHA.Name] -> [UHA.Constructor] -> Expr
 eqDict dictType dataType names constructors = foldr
     (Lam False)
     dictBody
-    (zipWith (\name idx -> Variable (idFromName name) $ Core.TAp typeDictEq $ Core.TVar idx) names [1 ..])
+    (zipWith
+        (\name idx ->
+            Variable (idFromName name) $ Core.TAp typeDictEq $ Core.TVar idx
+        )
+        names
+        [1 ..]
+    )
   where
     dictfType = Core.typeFunction [dictType, dataType, dataType] Core.typeBool
     dictBody  = let_
         (idFromString "func$eq")
         dictfType
         (eqFunction dictType dataType typeArgs constructors)
-        (Ap (Ap (ApType (Con $ ConId $ idFromString "Dict$Eq") dataType) (var "default$Eq$/=")) (var "func$eq"))
+        (Ap
+            (Ap (ApType (Con $ ConId $ idFromString "Dict$Eq") dataType)
+                (ApType (var "default$Eq$/=") dataType)
+            )
+            (var "func$eq")
+        )
     typeArgs = zipWith (\_ idx -> Core.TVar idx) names [1 ..]
 
 -- Example: data X a b = C a b Int | D Char b
@@ -69,9 +90,14 @@ eqFunction dictType dataType typeArgs constructors =
                        (map (makeAlt typeArgs) constructors)
                 )
             )
-    in  foldr (Lam False)
-              body
-              ([Variable (idFromString "dict") dictType, Variable fstArg dataType, Variable sndArg dataType]) -- \$fstArg $sndArg ->
+    in  foldr
+            (Lam False)
+            body
+            [ Variable (idFromString "dict") dictType
+            , Variable fstArg                dataType
+            , Variable sndArg                dataType
+            ]
+             -- \$fstArg $sndArg ->
 
 fstArg, sndArg :: Id
 [fstArg, sndArg] = map idFromString ["$fstArg", "$sndArg"]
@@ -95,15 +121,17 @@ makeAlt typeArgs constructor =
                 then Con (ConId (idFromString "True"))
                 else foldr1
                     andCore
-                    [ Ap (Ap (Ap (var "==") $ eqFunForType tp) (Var v)) (Var w) | (v, w, tp) <- zip3 vs ws types ]
+                    [ Ap (Ap (Ap (var "==") $ eqFunForType tp) (Var v)) (Var w)
+                    | (v, w, tp) <- zip3 vs ws types
+                    ]
             )
         , Alt PatDefault (Con (ConId (idFromString "False")))
         ]
     )
   where
     (ident, types) = nameAndTypes constructor
-    vs             = [ idFromString ("$v" ++ show i) | i <- [0 .. length types - 1] ]
-    ws             = [ idFromString ("$w" ++ show i) | i <- [0 .. length types - 1] ]
+    vs = [ idFromString ("$v" ++ show i) | i <- [0 .. length types - 1] ]
+    ws = [ idFromString ("$w" ++ show i) | i <- [0 .. length types - 1] ]
 {-  constructorToPat :: Id -> [UHA.Type] -> Pat
     constructorToPat id ts =
         PatCon (ConId ident) [ idFromNumber i | i <- [1..length ts] ] -}
@@ -111,9 +139,12 @@ makeAlt typeArgs constructor =
 
 nameAndTypes :: UHA.Constructor -> (Id, [UHA.Type])
 nameAndTypes c = case c of
-    UHA.Constructor_Constructor _ n ts -> (idFromName n, map annotatedTypeToType ts)
-    UHA.Constructor_Infix _ t1 n t2    -> (idFromName n, map annotatedTypeToType [t1, t2])
-    UHA.Constructor_Record _ _ _       -> error "pattern match failure in CodeGeneration.DerivingEq.nameAndTypes"
+    UHA.Constructor_Constructor _ n ts ->
+        (idFromName n, map annotatedTypeToType ts)
+    UHA.Constructor_Infix _ t1 n t2 ->
+        (idFromName n, map annotatedTypeToType [t1, t2])
+    UHA.Constructor_Record _ _ _ ->
+        error "pattern match failure in CodeGeneration.DerivingEq.nameAndTypes"
   where
     annotatedTypeToType :: UHA.AnnotatedType -> UHA.Type
     annotatedTypeToType (UHA.AnnotatedType_AnnotatedType _ _ t) = t
@@ -123,8 +154,9 @@ nameAndTypes c = case c of
 
 eqFunForType :: UHA.Type -> Expr
 eqFunForType t = case t of
-    UHA.Type_Variable    _ n      -> Var (idFromName n)
-    UHA.Type_Constructor _ n      -> var ("$dictEq$" ++ show n)
-    UHA.Type_Application _ _ f xs -> foldl Ap (eqFunForType f) (map eqFunForType xs)
-    UHA.Type_Parenthesized _ ty   -> eqFunForType ty
-    _                             -> internalError "DerivingEq" "eqFunForType" "unsupported type"
+    UHA.Type_Variable    _ n -> Var (idFromName n)
+    UHA.Type_Constructor _ n -> var ("$dictEq$" ++ show n)
+    UHA.Type_Application _ _ f xs ->
+        foldl Ap (eqFunForType f) (map eqFunForType xs)
+    UHA.Type_Parenthesized _ ty -> eqFunForType ty
+    _ -> internalError "DerivingEq" "eqFunForType" "unsupported type"
