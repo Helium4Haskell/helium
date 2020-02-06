4c0102d6aa00edbd0051d5bac38d5a57af2d3c5f
Mias van Klei <miasvanklei@outlook.com>
2020-02-06 19:30:12 +0100

    Helium.Utils.QualifiedTypes.Constants: move constants to Lvm.Constants

--- src/Helium/Utils/QualifiedTypes/Constants.hs
+++ src/Helium/Utils/QualifiedTypes/Constants.hs
@@ -1,12 +1,11 @@
 module Helium.Utils.QualifiedTypes.Constants where
 
 import Top.Types
+import Lvm.Constants
 
 ---------------------------------------------------------
 -- Qualified base types which are defined in LvmLang.core
      
-constants = ["Int", "Char", "String", "Float", "Bool"]
-
 {-Since we use qualified types now, all references to intType, charType, stringType, floatType and boolType should be changed to these-}
 intQualType, charQualType, stringQualType, floatQualType, boolQualType :: Tp
 intQualType    = TCon "Int"
