248ab43eeda97257ce54b937055a2206663b6a05
Hanno <hanno.ottens@hotmail.com>
2021-04-12 13:46:55 +0200

    Simple inlining idea

--- src/Helium/CodeGeneration/Iridium/RegionSize/Fixpoint.hs
+++ src/Helium/CodeGeneration/Iridium/RegionSize/Fixpoint.hs
@@ -11,7 +11,13 @@ import Helium.CodeGeneration.Iridium.RegionSize.Evaluate
 solveFixpoints :: Annotation -> Annotation
 solveFixpoints = eval . fillTop . foldAnnAlgLams fixAlg
     where fixAlg = idAnnAlg {
-        aFix = \d s as -> ATuple $ solveFixpoint d s as
+        aFix = \d s as -> ATuple $ solveFixpoint d s
+                                 . inlineFixpoint
+                                 . inlineFixpoint
+                                 . inlineFixpoint
+                                 . inlineFixpoint
+                                 . inlineFixpoint
+                                 $ inlineFixpoint as
     }
 
 -- | Solve a group of fixpoints
@@ -26,6 +32,46 @@ solveFixpoint d s fixes =
               in if ATuple res == state
                  then res
                  else fixIterate (n+1) (ATuple res) fs
+
+-- | Fill top with local variables in scope
+fillTop :: Annotation -> Annotation
+fillTop = go constrBot
+    where go scope (ATop   s c) = ATop s $ constrAdd c scope
+          go scope (ALam   s a) | sortIsRegion s = ALam s $ go (constrAdd (constrInfty $ AnnVar 0) (constrWeaken 1 scope)) a  
+                                | otherwise      = ALam s $ go (constrWeaken 1 scope) a
+          go scope (ATuple  as) = ATuple $ go scope <$> as
+          go scope (AProj  i a) = AProj i $ go scope a 
+          go scope (AApl   a b) = AApl   (go scope a) (go scope b) 
+          go scope (AAdd   a b) = AAdd   (go scope a) (go scope b)  
+          go scope (AMinus a r) = AMinus (go scope a) r
+          go scope (AJoin  a b) = AJoin  (go scope a) (go scope b)
+          go scope (AQuant a  ) = AQuant (go scope a)
+          go scope (AInstn a t) = AInstn (go scope a) t
+          go scope (AFix   s v) = AFix s $ go scope <$> v
+          go _     ann = ann
+
+----------------------------------------------------------------
+-- Fixpoint inlining
+----------------------------------------------------------------
+
+inlineFixpoint :: [Annotation] -> [Annotation]
+inlineFixpoint anns = let isRec = checkRecursive <$> anns
+                      in fillInNonRec isRec anns <$> anns
+
+fillInNonRec :: [Bool] -> [Annotation] -> Annotation -> Annotation 
+fillInNonRec isRec fixes = foldAnnAlgN 0 fillAlg
+    where fillAlg = idAnnAlg {
+        aProj = \d i a -> case a of
+                            AVar idx -> if idx == d && not (isRec !! i)
+                                        then fixes !! i
+                                        else AProj i $ AVar idx
+                            _ -> AProj i a
+    }
+
+
+-- | Check if a part of a fixpoint is recursive
+checkRecursive :: Annotation -> Bool
+checkRecursive ann = countFixBinds ann > 0 
 
 -- | Count usages of a variable
 countFixBinds :: Annotation -> Int
@@ -48,20 +94,3 @@ countFixBinds = foldAnnAlgN 0 countAlg
         aBot    = \_ _   -> 0,
         aFix    = \_ _ a -> sum a   
     }
-
--- | Fill top with local variables in scope
-fillTop :: Annotation -> Annotation
-fillTop = go constrBot
-    where go scope (ATop   s c) = ATop s $ constrAdd c scope
-          go scope (ALam   s a) | sortIsRegion s = ALam s $ go (constrAdd (constrInfty $ AnnVar 0) (constrWeaken 1 scope)) a  
-                                | otherwise      = ALam s $ go (constrWeaken 1 scope) a
-          go scope (ATuple  as) = ATuple $ go scope <$> as
-          go scope (AProj  i a) = AProj i $ go scope a 
-          go scope (AApl   a b) = AApl   (go scope a) (go scope b) 
-          go scope (AAdd   a b) = AAdd   (go scope a) (go scope b)  
-          go scope (AMinus a r) = AMinus (go scope a) r
-          go scope (AJoin  a b) = AJoin  (go scope a) (go scope b)
-          go scope (AQuant a  ) = AQuant (go scope a)
-          go scope (AInstn a t) = AInstn (go scope a) t
-          go scope (AFix   s v) = AFix s $ go scope <$> v
-          go _     ann = ann
