059a2402058509a798e3d6a63a246be0097a7d47
matty293 <60738079+matty293@users.noreply.github.com>
2024-11-11 15:05:36 +0000

    windows version to test helium

---
+++ test/compAndRun.ps1
@@ -0,0 +1,3 @@
+stack run -- --basepath=../lib/ --disable-logging --overloading $args[0]
+$path = [System.IO.Path]::GetDirectoryName($args[0])
+Get-ChildItem -Path $path -Recurse -Include *.desugared.core, *.iridium, *.ll, *.test.core, *.exe | Remove-Item -Force
