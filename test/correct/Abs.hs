9e114ea5eb45f875d0eb3b0ef6edadad1e3108bd
matty293 <60738079+matty293@users.noreply.github.com>
2024-11-11 12:59:43 +0000

    compile and run updated

--- test/correct/Abs.hs
+++ test/correct/Abs.hs
@@ -1,3 +1,5 @@
 module Abs where
 
-main = (abs 3, abs (-4), absFloat 4.0, absFloat (-.2.0))
+--import Prelude
+
+main = ("LvmLang is up to date\nLvmException is up to date\nHeliumLang is up to date\nPreludePrim is up to date\nPrelude is up to date\nCompiling correct/Abs.hs\n(5,26): Undefined variable \"absFloat\"\n(5,40): Undefined variable \"absFloat\"\nCompilation failed with 2 errors")
